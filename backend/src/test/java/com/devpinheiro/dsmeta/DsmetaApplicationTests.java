0dd2ac55cc0ea7c22995876590782016c7dd3284
Aryel Pinheiro <aryelpinheiro2@gmail.com>
2022-11-14 11:50:37 -0300

    Project created

---
+++ backend/src/test/java/com/devpinheiro/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpinheiro.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
